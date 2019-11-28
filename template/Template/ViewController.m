b832534177aa98aabe3f389f3e95e1c1f46747b7
oceanbao <oceanbao@abiechina.com>
2019-11-28 18:09:49 +0800

    update template

--- template/Template/ViewController.m
+++ template/Template/ViewController.m
@@ -19,8 +19,21 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
-    /*
-     父类规定处理的流程，子类实现具体的处理
+    /* 模板 Template
+     
+     定义：父类规定处理的流程，子类实现具体的处理
+     
+     举例：
+     打开微博：拿起手机->解锁->打开微博，流程是这样的，具体你怎么操作（解锁、打开微博）就交给子类
+     
+     角色：
+        * 抽象类：定义处理流程
+        * 具体类：实现具体操作
+     
+     相关联的设计模式：
+        * 工厂模式 Factory：模板模式的使用
+        * 策略模式 Strategy:
+     
      模板不用 protocol 的原因是，模板可以实现一些方法，protocol 不能
      */
     ConcreteTemplate *template = [[ConcreteTemplate alloc] init];
